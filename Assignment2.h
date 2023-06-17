cec5d73df978bba6b31cbda5b822cf268db97c0e
Matthew <wppmat001@myuct.ac.za>
2023-06-17 20:55:18 +0200

    adding something

--- Assignment2.h
+++ Assignment2.h
@@ -14,6 +14,12 @@ namespace WPPMAT001{
          int size();
          unsigned char ** getFrame(int frame);
          std::vector<unsigned char **> getSequence();
+         void read(std::string filename, int xorigion, int yorigion, int xend, int yend, int height, int width);
+         void write(int height, int width);
+         void reverse(int height, int width);
+         void invert(int height, int width);
+         void reverse_invert(int height, int width);
+         
    
    };
    
