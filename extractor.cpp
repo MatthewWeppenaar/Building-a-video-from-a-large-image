da55e82eb613673c6fef2c63a92c153004de0deb
Matthew <wppmat001@myuct.ac.za>
2023-06-23 00:04:08 +0200

    adding multipoint traversal handelling

--- extractor.cpp
+++ extractor.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <cmath>
 #include <vector>
+#include <algorithm>
 //used to store frame writing options
 struct options {
     std::string operation;
@@ -10,24 +11,53 @@ struct options {
 };
 int main(int argc, char *argv[]){
   WPPMAT001::FrameSequence image;
-  int xs = std::stoi(argv[3]);//reading start and end points
-  int ys = std::stoi(argv[4]);
-  int xe = std::stoi(argv[5]);
-  int ye = std::stoi(argv[6]);
-  int frameheight = std::stoi(argv[8]);//frame dimensions
-  int framewidth  = std::stoi(argv[9]);
+  std::cout<<argv[2]<<std::endl;
+  
+  //finding the indexes of '-s' and '-w' in argv
+  int dimensions = -1;
+  int operations = -1;
+
+for (int i = 0; i < argc; i++) {
+    if (std::string(argv[i]) == "-s") {
+        dimensions = i;
+    } else if (std::string(argv[i]) == "-w") {
+        operations = i;
+        i+= argc;//to ensure that only the first '-w' is stored
+    }
+}
+
+  int frameheight = std::stoi(argv[dimensions+1]);//frame dimensions
+  int framewidth  = std::stoi(argv[dimensions+2]);
   
   std::vector<options> frameOptions;
 
   std::string imagefilename;
   imagefilename = argv[1];
-  std::vector<WPPMAT001::origion_coords> coords= image.generate_trajectory(xs,ys,xe,ye);
-  image.read(imagefilename);//creating frames along trajectory
-  image.generate_frame_sequence(frameheight,framewidth,coords);
-  
-  
+  //reading in the input image data
+  image.read(imagefilename);
+  //single point trajectory
+  if(std::string(argv[2]) == "-t"){
+   int xs = std::stoi(argv[3]);//reading start and end points
+   int ys = std::stoi(argv[4]);
+   int xe = std::stoi(argv[5]);
+   int ye = std::stoi(argv[6]);
+   std::vector<WPPMAT001::origion_coords> coords= image.generate_trajectory(xs,ys,xe,ye);
+   //creating frames along trajectory
+   image.generate_frame_sequence(frameheight,framewidth,coords);
+  }
+  else if(std::string(argv[2]) == "-p"){
+   std::vector<WPPMAT001::origion_coords> coords;
+   int num_points = std::stoi(argv[3]);
+   for(int i = 4; i<2+ num_points *2;i+=2){
+      std::vector<WPPMAT001::origion_coords> coords1 = image.generate_trajectory(std::stoi(argv[i]),std::stoi(argv[i+1]),std::stoi(argv[i+2]),std::stoi(argv[i+3]));
+      coords.insert(coords.end(), coords1.begin(), coords1.end());
+   }
+   
+   image.generate_frame_sequence(frameheight,framewidth,coords);
+  }
+  std::cout << image.size() << std::endl;
   //collecting all the frame operations into a vector
-  for(int i=10;i<argc; i++){
+  for(int i=operations;i<argc; i++){
    options frame;
    frame.operation = argv[++i];
    frame.name = argv[++i];
